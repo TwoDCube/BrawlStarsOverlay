147bd6039c8d2dfd79ba1838d3a2ce09d1681e77
TwoDCube <2dcube01@gmail.com>
2019-03-22 21:18:56 +0100

    Add controls firebase

--- src/app/control/control.component.ts
+++ src/app/control/control.component.ts
@@ -10,15 +10,37 @@ import {AngularFirestore} from '@angular/fire/firestore';
 export class ControlComponent implements OnInit {
 
   fg = this.fb.group({
-    team1: this.fb.control(null),
-    score1: this.fb.control(null),
-    score2: this.fb.control(null),
-    team2: this.fb.control(null)
+    show: true,
+    team1: '',
+    score1: 0,
+    score2: 0,
+    team2: ''
   })
+
+  teams;
 
   constructor(private fb: FormBuilder, private afStore: AngularFirestore) { }
 
   ngOnInit() {
+    this.afStore.collection('info').doc('scores').valueChanges().subscribe((res: any) => {
+      this.fg.patchValue({
+        show: res.show,
+        team1: res.team1,
+        score1: res.score1,
+        score2: res.score2,
+        team2: res.team2
+      })
+    })
+    this.fg.valueChanges.subscribe(async res => {
+      await this.afStore.collection('info').doc('scores').update({
+        show: res.show,
+        team1: res.team1,
+        score1: res.score1,
+        score2: res.score2,
+        team2: res.team2
+      })
+    })
+    this.teams = this.afStore.collection('teams').valueChanges()
   }
 
 }
